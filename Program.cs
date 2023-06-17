cc8694a9ad45cd1829bd63b100ec44aecacbed57
cobbat <sofiaveron01@gmail.com>
2023-06-16 23:48:40 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Mi nombre
+Console.WriteLine("Escribí tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+//Que hora es?
+Console.WriteLine("Escribir qué hora es");
+var hora = Console.ReadLine();
+Console.WriteLine("Son las "+hora+"hs.");
